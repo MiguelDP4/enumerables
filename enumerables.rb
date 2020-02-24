a97881db88fc6a0a328a91da41b1a9be058c9efc
MiguelDP4 <migueldp4@gmail.com>
2020-02-24 08:31:19 -0600

    create my_each_with_index method, create test for my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -4,6 +4,14 @@ module Enumerable
       yield(value)
     end
   end
+
+  def my_each_with_index
+    i = 0
+    for value in self do
+      yield(value, i)
+      i += 1
+    end
+  end
 end
 
 testing_array = [11, 15, 18, 54, 23, 46, 96, 85, 42, 10, 36, 52]
@@ -11,4 +19,8 @@ print 'Test array: ', testing_array
 puts ''
 puts 'Testing my_each: '
 testing_array.my_each { |value| print value * 2, ' ' }
+puts ''
+
+puts 'Testing my_each_with_index: '
+testing_array.my_each_with_index { |value, index| puts "Value: #{value}, Index: #{index}" }
 puts ''
