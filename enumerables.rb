321e9080f23ce128b08fb04d73238435299dc69d
MiguelDP4 <migueldp4@gmail.com>
2020-02-24 08:33:25 -0600

    create my_select method, create test for my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -12,6 +12,14 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_select
+    new_array = []
+    for value in self do
+      new_array.push(value) if yield(value)
+    end
+    new_array
+  end
 end
 
 testing_array = [11, 15, 18, 54, 23, 46, 96, 85, 42, 10, 36, 52]
@@ -23,4 +31,8 @@ puts ''
 
 puts 'Testing my_each_with_index: '
 testing_array.my_each_with_index { |value, index| puts "Value: #{value}, Index: #{index}" }
+puts ''
+
+puts 'Testing my_select by selecting odd numbers'
+testing_array.my_select { |value| print "#{value} " if value.even? }
 puts ''
